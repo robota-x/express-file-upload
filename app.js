818b85d3d104a0edeaf07dbc67ee8148ebbe7a5a
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-11-21 23:12:28 +0000

    extra route to echo back the sent text

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 var path = require('path');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 var SERVER_PORT = 8000;
+
+var userText = 'No text has been sent, this is the default value';
+
+app.use(bodyParser.urlencoded());
 
 
 app.get('/', function(req, res) {
   res.status(200).sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.post('/postdata', function(req, res) {
-  res.status(200).send('received data');
+app.post('/post-text', function(req, res) {
+  res.status(200).sendFile(path.join(__dirname + '/success.html'));
+  console.log('received this parameters: ', req.body);
+  console.log('the first letter of the text var is: ', req.body.text[0]);
+
+  console.log(req.body.text + ' is also now saved in the backend as the "userText" var');
+  userText = req.body.text;
 });
+
+app.get('/user-text', function(req, res) {
+  res.status(200).send(userText);
+});
+
 
 app.listen(SERVER_PORT, function() {
   console.log('server started on port ', SERVER_PORT);
